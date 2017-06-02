08d3e0a4a2cb0eb89f4bf36a07641d54b1b4c5de
AlexanderOsokin <tureckiy@gmail.com>
2017-06-02 09:32:38 +0300

    ifdef for macos

--- FaustGames/Native/src/graphics/graphics_platform.h
+++ FaustGames/Native/src/graphics/graphics_platform.h
@@ -2,16 +2,18 @@
 #include <jni.h>
 #include <GLES2/gl2.h>
 #include <android/log.h>
-#else
-#ifdef __APPLE__
+#elif __APPLE__
+#include "TargetConditionals.h"
+#if TARGET_OS_IPHONE
 #include <OpenGLES/ES2/gl.h>
 #include <OpenGLES/ES2/glext.h>
+#elif TARGET_OS_MAC
+#include <OpenGL/gl.h>
+#endif
 #else
 #include "..\..\llge\stdafx.h"
 #include <windows.h>
 #include "glew.h"
 #endif
-#endif
-
 #include <string>
 #include <vector>
